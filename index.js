0311de3262029417bf5ee26ac46ea20bc3b456f4
unknown <Fanor@FDN-WS-023166.acad.dvuadmin.net>
2018-01-26 21:51:43 -0300

    Segundo desafio

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var n = process.argv[2];
+    const a = x => {
+        if(Number(x)%2 == 0)
+             console.log('PAR')
+        else
+             console.log('IMPAR')
+        return
+    }
+a(n);
+
